2f5c7e67d8f8705a92404b25c3136446c7b599e9
rohitmaisale12 <rohitmaisale12@gmail.com>
2026-03-10 12:05:17 +0530

    Add program to calculate average of three numbers

---
+++ basics/average_three_numbers.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout<<"enter the 3 numbers=";
+    cin>>a>>b>>c;
+    float average=(a+b+c)/3.0;
+    cout<<"average="<<average;
+    return 0;
+}
