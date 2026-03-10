e9af6c5b14d0202f362751768520fd38d94613e7
rohitmaisale12 <rohitmaisale12@gmail.com>
2026-03-10 11:37:39 +0530

    Implement sum of two numbers in C++
    Fix input/output formatting and calculate sum correctly.

---
+++ basics/sum_two_numbers.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    int b;
+    cout<<"enter the 2 numbers=";
+    cin>>a>>b;
+    int sum;
+    cout<<"sum="<<a+b;
+    return 0;
+}
